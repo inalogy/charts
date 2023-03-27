11c47a87ef04e5c04584cc935a81c4954420b837
Martin Handl <martin.handl@inalogy.com>
2023-03-27 11:10:37 +0200

    extraEnvVarsCMextraEnvVarsCM

--- inalogy/midpoint/config/sql/101-dwh-org-chart.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "sample_org_chart" (
-     code varchar NOT NULL,
-     org_unit varchar NOT NULL,
-     parent_code varchar NULL,
-     CONSTRAINT "sample_org_chart_pk" PRIMARY KEY (code)
-);
-
--- inalogy/midpoint/config/sql/102-dwh-position.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "sample_position" (
-    code varchar NOT NULL,
-    "position" varchar NOT NULL,
-    org_unit_code varchar NOT NULL,
-    gestor varchar NULL,
-    "user" varchar NULL,
-    CONSTRAINT "sample_position_pk" PRIMARY KEY (code)
-);
